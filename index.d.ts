3b4cefd4a05f563379a5aeb6786430c1a549a208
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2018-11-26 20:08:22 +0330

    run prettier on index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
 declare module 'jsx-xml' {
-
-  interface DeepArray<T> extends Array<DeepArrayValue<T>> {
-  }
+  interface DeepArray<T> extends Array<DeepArrayValue<T>> {}
 
   type DeepArrayValue<T> = T | DeepArray<T>
 
@@ -11,32 +9,36 @@ declare module 'jsx-xml' {
   type Children = DeepArrayValue<Child>
 
   interface Options {
-    xmldec?: any,
-    doctype?: any,
-    createOptions?: any,
-    endOptions?: any,
+    xmldec?: any
+    doctype?: any
+    createOptions?: any
+    endOptions?: any
   }
 
-  function render(jsx: Children, options?: Options): string;
+  function render(jsx: Children, options?: Options): string
 
-  function JSXXML(type: string | Function, attr: null | object, ...children: Children[]): Children;
+  function JSXXML(
+    type: string | Function,
+    attr: null | object,
+    ...children: Children[]
+  ): Children
 
-  function Fragment(props: { children: DeepArray<any> }): Children[];
+  function Fragment(props: { children: DeepArray<any> }): Children[]
 
-  function CData(props: { children: DeepStringArray }): Child;
+  function CData(props: { children: DeepStringArray }): Child
 
-  function Comment(props: { children: DeepStringArray }): Child;
+  function Comment(props: { children: DeepStringArray }): Child
 
-  function Raw(props: { children: DeepStringArray }): Child;
+  function Raw(props: { children: DeepStringArray }): Child
 
   interface jsxxml {
-    render,
-    JSXXML,
-    Fragment,
-    CData,
-    Comment,
-    Raw,
+    render
+    JSXXML
+    Fragment
+    CData
+    Comment
+    Raw
   }
 
-  export = jsxxml;
+  export = jsxxml
 }
